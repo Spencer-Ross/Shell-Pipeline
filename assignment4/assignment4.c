e881ce41e71b272c23849be88671414c334716ba
Spencer <spencerthomasr@gmail.com>
2020-02-19 14:33:05 -0800

    init commit

---
+++ assignment4/assignment4.c
@@ -0,0 +1,23 @@
+/***********************************************************************
+name:
+	assignment4 -- acts as a pipe using ":" to seperate programs.
+description:	
+	See CS 360 Processes and Exec/Pipes and Signals lecture for helpful tips.
+***********************************************************************/
+
+/* Includes and definitions */
+
+/**********************************************************************
+./assignment4 <arg1> : <arg2>
+
+    Where: <arg1> and <arg2> are optional parameters that specify the programs
+    to be run. If <arg1> is specified but <arg2> is not, then <arg1> should be
+    run as though there was not a colon. Same for if <arg2> is specified but
+    <arg1> is not.
+**********************************************************************/
+
+
+int main(int argc, char *argv[]){
+	/* code */
+	return 0;
+}
